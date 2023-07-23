7e715c19acaa563e34df7003f1a8dcbe9166850f
alisaLachman <alisal@wix.com>
2023-07-23 17:51:27 +0300

    padding and layout changes

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import styles from './card.module.scss';
-import { Icon } from '../icon/icon';
 
 export interface CardProps {
     className?: string;
@@ -13,10 +12,9 @@ export interface CardProps {
 export const Card = ({ className }: CardProps) => {
     return (
         <div className={classNames(styles.root, className)}>
-            <Icon />
             <div className={styles.text_container}>
-                <p className={styles.card_paragraph}>This is a paragraph.</p>
-                <h4 className={styles.card_headline}>Heading 4</h4>
+                <h4 className={styles.card_headline}>Pay someone</h4>
+                <p className={styles.card_paragraph}>To wallet, bank or mobile number</p>
             </div>
         </div>
     );
