b13082695ecd53752020a1e2a73bcd1faf7a5614
alisaLachman <alisal@wix.com>
2023-07-23 16:31:06 +0300

    .

--- src/_codux/boards/icon/flower-icon.board.tsx
+++
@@ -1,7 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { Icon } from '../../../components/icon/icon';
-
-export default createBoard({
-    name: 'fire',
-    Board: () => <Icon />,
-});
