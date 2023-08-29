5c7ec04aac7847eda83a12e34b789be7976d82f3
alisaLachman <alisal@wix.com>
2023-08-29 09:53:45 +0300

    Create one row of flower icons

--- src/components/icon-grid/icon-grid.tsx
+++ src/components/icon-grid/icon-grid.tsx
@@ -7,25 +7,18 @@ export interface IconGridProps {
 }
 
 const icons: IconType[] = ['cloud', 'flower', 'heart', 'duck'];
-const demoArray = [
-    { abc: 'root', anotherKey: 'anotherValue' },
-    { abc: 'whatever', anotherKey: 'anotherValue', yetAnotherKey: 'anotherValue' },
-];
+
 
 export const IconGrid = ({ className }: IconGridProps) => {
     return (
         <div className={classNames(styles.root, className, 'IconGrid')}>
             {icons.map((icon) => {
                 const whatever = 'hello';
-                if (icon === 'cloud') {
-                    return <Icon iconType="cloud" />;
-                }
+               
                 return <Icon iconType="flower" />;
             })}
 
-            {demoArray.map((obj) => {
-                return <>{obj.abc}</>;
-            })}
+           
         </div>
     );
 };
